ff2e6dbb57e2fdeeb3c8d57d767e8e44c7a0fb24
emascapa <emascapa@gmail.com>
2022-06-08 16:41:02 +0200

    Add home custom page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -81,19 +81,17 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    CIAO CARI
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+
+                    @foreach ($nav_links as $name => $link)
+                    <a href="{{$link}}">{{$name}}</a>
+                    @endforeach
+
                 </div>
+
             </div>
         </div>
     </body>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    //parametri da passare
+    $nav_links = [
+        'Bulbasaur' => 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png',
+        'Charmender' => 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png',
+        'Squirtle' => 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png',
+        'Chikorita' => 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/152.png',
+        'Cyndaquil' => 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/155.png',
+        'Totodile' => 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/158.png'
+    ];
+
+    return view(
+        'home',
+        compact('nav_links')
+    );
 });
